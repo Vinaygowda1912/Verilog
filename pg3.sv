ec29f25b2f2e040001b3b9da4707af59abc7d80e
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-12 15:09:35 +0530

    Update pg3.sv

--- pg3.sv
+++ pg3.sv
@@ -2,11 +2,11 @@ module d_ff ( d,q,set,clk);
   input d,t,clk,set;
   output reg q;
   always @ (posedge clk)
-    begin
-      if(clk==1)
         begin 
           if(set==1)
-            q=1;
+            q<=1;
+          else
+            q<=d;
         end 
       endmodule
 
