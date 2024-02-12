6deb512c63f750940213cfe381795a27d793b680
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-12 11:34:03 +0530

    Update Program 1.sv

--- Program 1.sv
+++ Program 1.sv
@@ -1,1 +1,11 @@
-module fulladder (a,b);
+module d_ff (d,clk,reset,q);
+  input d,clk,reset;
+  output reg q;
+  always @(posedge clk)
+    begin
+      if (reset==1'b1)
+        q<=1'b0;
+      else
+        q<=d;
+    end 
+endmodule
