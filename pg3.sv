570ea2880bceb52b79c9b6a388e66420b9e16593
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-12 13:18:46 +0530

    Update pg3.sv

--- pg3.sv
+++ pg3.sv
@@ -9,4 +9,28 @@ module d_ff ( d,q,set,clk);
             q=1;
         end 
       endmodule
-  
+
+
+      module d_fft;
+  reg d,set,clk;
+  wire q;
+  d_ff dut(.d(d),,set(set),.clk(clk),.q(q));
+     always#10
+      clk=~clk;
+   initial begin
+     clk=0;
+     set=0;
+     d=0;
+     #10
+     d=1;
+     set=0;
+     set=1;
+     #10
+     d=0;
+     set=0;
+     set=1;
+     $finish;
+   
+  end
+endmodule
+     
