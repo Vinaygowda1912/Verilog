32b91f8db047c4c81dc620d7c913a4c9740429dc
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-12 12:13:03 +0530

    Update Program 1.sv

--- Program 1.sv
+++ Program 1.sv
@@ -1,5 +1,5 @@
 module d_ff (d,clk,reset,q);
-  input d,clk,reset;
+  input d,clk,reset;                          
   output reg q;
   always @(posedge clk)
     begin
@@ -9,3 +9,26 @@ module d_ff (d,clk,reset,q);
         q<=d;
     end 
 endmodule
+
+
+module dff_t();
+  reg d,clk,reset;
+  wire q;
+  d_ff d1 (d,clk,reset,q);
+  initial begin
+    clk=1'b0;
+    forever 
+      #10
+      clk=~clk;
+  end
+  initial begin
+    reset=1'b1; 
+    d=1'b0;
+    #20
+    reset=1'b0;
+    d=1'b1;
+  $finish;
+  end 
+endmodule
+    
+    
