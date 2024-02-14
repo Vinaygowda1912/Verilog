0b6f32cac2e7de71320b675d68594cf66c5dfbff
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-14 16:12:45 +0530

    Update 25.sv

--- 25.sv
+++ 25.sv
@@ -1,5 +1,6 @@
-module shift_reg(d,clk,si,so);
-  input d,clk,si;
+module shift_reg(d,clk,load,si,so);
+  input [7:0]d;
+  input load,clk,si;
   output so;
   reg[7:0] temp;
   always @ (posedge clk)
