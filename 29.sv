6cf5e406248d79dab8c5dde7576adb39bbd8f2da
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-14 20:19:52 +0530

    Create 29.sv

---
+++ 29.sv
@@ -0,0 +1,15 @@
+module mux (a,b,c,d,s,y);
+  input a,b,c,d;
+  input [1:0]s;
+  output y;
+  reg y;
+  always @ (s)
+    begin
+      case(s)
+        2'b00:y=a;
+        2'b01:y=b;
+        2'b10:y=c;
+        2'b11:y=d
+          endcase
+          end
+          endmodule
