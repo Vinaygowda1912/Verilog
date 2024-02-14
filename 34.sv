247e0f1d1fcb155ed8ca4932c54a6de3b1ec48b2
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-14 21:19:05 +0530

    Create 34.sv

---
+++ 34.sv
@@ -0,0 +1,16 @@
+module logical shift (di,sel,o);
+  input [7:0]di;
+  input [1:0]sel;
+  output [7:0]o;
+  reg [7:0]o;
+  always @ (*)
+    begin
+      2'b00:o=d1;
+      2'b01:o=d1<<1;
+      2'b10:o=d2<<2;
+      default:o=d3<<3;
+      endcase
+    end 
+endmodule
+
+      
