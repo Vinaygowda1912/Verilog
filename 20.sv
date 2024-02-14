d9b9b0aa120499f691d2a99b3f0866b7ce3a4451
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-14 15:48:10 +0530

    Create 20.sv

---
+++ 20.sv
@@ -0,0 +1,12 @@
+module shift_reg( clk,si,so);
+input clk;
+  input[7:0] si;
+  output[7:0] so;
+  reg [7:0] temp;
+  always @ (posedge clk)
+    begin
+      temp<=temp<<1;
+      temp<=si;
+    end
+  assign s0=temp
+    endmodule
