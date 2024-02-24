640139b5490a5931e25c8c3a541bb54da1e6b2cc
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-24 19:30:36 +0530

    Update 28.sv

--- 28.sv
+++ 28.sv
@@ -14,5 +14,39 @@ y=c;
 else
 y=d;
 end 
-endmoduke
+endmodule
 
+
+
+module muxt();
+  reg a,b,c,d;
+  reg [1:0]s;
+  wire o;
+  mux dut(.a(a),.b(b),.c(c),.d(d),.s(s),.o(o));
+ 
+  initial begin 
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    s=2'b00;
+    a=0;
+    b=0;
+    c=0;
+    d=0;
+    #5
+    a=1;
+    #3
+    b=1;
+    #5
+    c=1;
+    #5
+    d=1;
+    #5
+    s=2'b01;
+    #5
+    s=2'b10;
+    #5
+    s=2'b11;
+    #100
+    $finish();
+  end
+endmodule
