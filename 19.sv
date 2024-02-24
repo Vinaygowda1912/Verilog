c3adb26adebe2926b89b7d44dd0afaf1729806eb
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-24 11:53:54 +0530

    Update 19.sv

--- 19.sv
+++ 19.sv
@@ -12,4 +12,29 @@
         end
            assign q = temp;
         endmodule
+
+module countert();
+  reg clk,clr;
+  reg [3:0]d;
+  wire [3:0]q;
+  accum dut(.clk(clk),.clr(clr),.d(d),.q(q));
+  always #5 clk=~clk;
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    
+    clk=0;
+    clr=1;
+    d=0;
+    #5
+    clr=0;
+    #5
+    d=1;
+    #100
+ 
+    
+    $finish();
+    end
+endmodule
+        
         
