a9e5f34081641d0dcf02d4cd934f3f7c52911637
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-24 11:33:53 +0530

    Update 15.sv

--- 15.sv
+++ 15.sv
@@ -11,4 +11,33 @@ module counter (clk, clr, ce, q);
 	end
 	   assign q = tmp;
         endmodule
+
+
+module countert();
+  reg clk,clr,ce;
+  wire [3:0]q;
+  counter dut(.clk(clk),.clr(clr),.ce(ce),.q(q));
+  
+  always #5 clk=~clk;
+   always #5 ce=~ce;
+
+  
+  initial begin
+    
+    $dumpfile("dump.vcd");
+    $dumpvars(0);
+    clk=0;
+    clr=1;
+    ce=1;
+    #5
+    clr=0;
+    #5
+    ce=0;
+
+    #100
+ 
+    
+    $finish();
+    end
+endmodule
         
