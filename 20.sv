ca180318466d7659d2ccde19bcad6472adcbdd85
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-24 12:39:37 +0530

    Update 20.sv

--- 20.sv
+++ 20.sv
@@ -10,3 +10,40 @@ input clk;
     end
   assign s0=temp[7]
     endmodule
+
+
+    module shift_regt();
+  reg clk,clr;
+  reg[2:0] si;
+  wire[2:0] so;
+  shift_reg dut(.clk(clk),.clr(clr),.si(si),.so(so));
+  
+  always #5 clk=~clk;
+  
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    
+    clk=0;
+    clr=1;
+    si=3'b000;
+    
+    #5
+    clr=0;
+    #5
+    clr=1;
+   #5
+    si=3'b001;
+    #5
+    si=3'b010;
+    #5
+    si=3'b011;
+    #5
+    si=3'b100;
+    #100
+ 
+    
+    $finish();
+    end
+endmodule
+        
