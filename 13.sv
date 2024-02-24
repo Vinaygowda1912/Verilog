8eeff4944cac5aab18dfb56406e379abeb80f071
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-24 11:10:29 +0530

    Update 13.sv

--- 13.sv
+++ 13.sv
@@ -12,4 +12,31 @@ module counter (clk,d,load,q);
     end
   assign q = temp;
 endmodule
+
+
+module countert();
+  reg clk,load,d;
+  wire [3:0]q;
+  counter dut (.clk(clk),.d(d),.load(load),.q(q));
+  
+  always #5 clk=~clk;
+
+  
+  initial begin
+    
+    $dumpfile("dump.vcd");
+    $dumpvars(0);
+    clk=0;
+    d=0;
+    load=1;
+    #5
+    load=0;
+    #5
+    d=1;
+    #100
+    
+    $finish();
+    end
+endmodule
+        
       
