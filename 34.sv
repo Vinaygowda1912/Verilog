8b2578a6090271a908be73ec9b5d4ea002bb3f97
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-24 20:40:37 +0530

    Update 34.sv

--- 34.sv
+++ 34.sv
@@ -14,4 +14,31 @@ module logical shift (di,sel,o);
     end 
 endmodule
 
+
+
+module logicalshift_t() ;
+  reg[7:0] di;
+  reg[1:0] sel;
+  wire[7:0] o;
+  logicalshift dut(.di(di),.sel(sel),.o(o));
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+   di=8'b10110010;
+    #5
+ sel=2'b00;
+    #5
+    sel=2'b01;
+    #5
+    sel=2'b10;
+    #5
+    sel=2'b11;
+    #100
+    
+    
+   
+    $monitor("di=%b,sel=%b,0=%b",di,sel,o);
+    $finish();
+  end
+endmodule
       
