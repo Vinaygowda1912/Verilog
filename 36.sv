29e95e7ad9805b6389ed9a8e7687e3fecf1f4960
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-25 10:28:15 +0530

    Update 36.sv

--- 36.sv
+++ 36.sv
@@ -1,9 +1,24 @@
-module co (a,b,sum,cout);
+module co(a,b,sum,cout);
   input [7:0]a,b;
   input [7:0]sum;
   output cout;
-  wire [7:0]temp;
-  assign temp = a+b;
-  assign sum = temp[7:0];
-  assign cout = temp[8];
+ 
+  assign {cout,sum} = a+b;
 endmodule 
+
+module co_t();
+  reg[7:0] a,b;
+  reg[7:0] sum;
+  wire[7:0] cout;
+    co dut(.a(a),.b(b),.sum(sum),.cout(cout));
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+  a=8'b11111111;
+    b=8'b00000000;
+  
+    #100
+    $monitor("a=%b,b=%b,sum=%b,cout=%b",a,b,sum,cout);
+    $finish();
+  end
+endmodule
