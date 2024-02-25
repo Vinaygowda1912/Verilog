a8a6cc613d30623b0f969377ed5bb038d4a646bd
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-25 09:51:11 +0530

    Update 35.sv

--- 35.sv
+++ 35.sv
@@ -8,3 +8,23 @@ module adder carry_in (a,b,cin,sum);
       sum = a+b+cin;
     end
 endmodule
+
+
+module addercarry_int();
+  reg[7:0] a,b;
+  reg cin;
+  wire[7:0] sum;
+  addercarry_in dut(.a(a),.b(b),.cin(cin),.sum(sum));
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    a=8'b01001010;
+    #10
+    b=8'b01010011;
+    #6
+    cin=0;
+    #100
+    $monitor("a=%b,b=%b,cin=%b,sum=%b",a,b,cin,sum);
+    $finish();
+  end
+endmodule
