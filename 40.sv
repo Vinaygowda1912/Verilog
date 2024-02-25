e3b71f47e3c293ac177cd68520507b52612a4b3d
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-25 12:46:10 +0530

    Update 40.sv

--- 40.sv
+++ 40.sv
@@ -4,3 +4,19 @@ module mult (a,b,y);
   output [11:0]y;
   assign y = a*b;
 endmodule
+
+module mult_t();
+  reg[7:0] a;
+  reg[3:0] b;
+  wire[11:0] y;
+  mult dut(.a(a),.b(b),.y(y));
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+ a=5;
+    b=6;
+    #100
+    $monitor("a=%d,b=%d,y=%d",a,b,y);
+    $finish();
+  end
+endmodule
