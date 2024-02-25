7c86b0ca46dfffbdc801ea9a95f7cf26f0f010ce
Vinaygowda1912 <159707793+Vinaygowda1912@users.noreply.github.com>
2024-02-25 12:20:40 +0530

    Update 38.sv

--- 38.sv
+++ 38.sv
@@ -10,3 +10,24 @@ module add/sub (a,b,oper,y);
       else y = a-b;
     end
 endmodule
+
+
+module add_t();
+  reg[7:0] a,b;
+  reg[1:0] oper;
+  wire[7:0] y;
+  add dut(.a(a),.b(b),.oper(oper),.y(y));
+  initial begin
+    $dumpfile("dump.vcd");
+    $dumpvars(1);
+    oper=2'b00;
+    a=5;
+    b=1;
+    #5
+    oper=2'b01;
+    a=6;
+    b=1;
+    #100
+    $monitor("a=%d,b=%d,y=%d",a,b,y);
+  end
+endmodule
